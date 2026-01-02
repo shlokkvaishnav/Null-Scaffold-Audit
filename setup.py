9833edf6e766299a4ab4bbff7c383a5a575145cf
shlokkvaishnav <shlok9640@gmail.com>
2026-01-02 14:42:16 +0530

    Simplify repository structure

--- setup.py
+++
@@ -1,7 +0,0 @@
-"""Setup script for climate_discovery package."""
-from setuptools import setup
-
-# This file is kept for backwards compatibility
-# The package is configured via pyproject.toml
-setup()
-
