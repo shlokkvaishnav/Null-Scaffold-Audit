eb90ed9dd9659b28c3acbd0225802bb2df8367e4
shlokkvaishnav <shlok9640@gmail.com>
2026-01-07 20:53:44 +0530

    feat: Week 4 Variable Discovery (L1 Sparse Gating)

--- src/climate_discovery/models/loss.py
+++ src/climate_discovery/models/loss.py
@@ -21,3 +21,30 @@ class RegimeConsistencyLoss(nn.Module):
         loss = diff_h.mean() + diff_w.mean()
 
         return self.weight * loss
+
+class L1SparseLoss(nn.Module):
+    def __init__(self, weight=0.01):
+        super().__init__()
+        self.weight = weight
+
+    def forward(self, model):
+        """
+        Calculates L1 loss on the weights of the *first layer* of the model.
+        This encourages sparsity in feature selection.
+        """
+        l1_reg = torch.tensor(0., requires_grad=True)
+        
+        # We assume the first layer is the one connected to inputs
+        # For GatingNetwork, it is model.net[0] (Linear)
+        # We iterate to find the first Linear layer just to be safe/generic
+        for name, param in model.named_parameters():
+            if 'weight' in name:
+                l1_reg = l1_reg + torch.norm(param, 1)
+                # We only want the FIRST layer for feature selection sparsity
+                # But typically L1 on all layers is fine for general sparsity.
+                # For specific "Variable Selection", we care most about the input weights.
+                # Let's target the input layer specifically if possible, but general L1 is okay too.
+                # To be precise for "Variable Discovery", we should punish the Input->Hidden weights strongly.
+                break # ONLY punish the first layer found!
+        
+        return self.weight * l1_reg
